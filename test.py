f615c8482f1f78afe4a0d45bb2160eba0f9de9ff
amitdubey90 <amit.dubey2219@gmail.com>
2014-12-05 23:55:06 -0800

    added date to responses

--- test.py
+++ test.py
@@ -2,8 +2,10 @@ from bottle import route, run, request
 from pymongo import MongoClient
 import json
 import MySQLdb
+import datetime
+from bson import json_util
 
-client = MongoClient('ec2-54-67-57-228.us-west-1.compute.amazonaws.com', 27017)
+client = MongoClient('localhost', 27017)
 mongoDB = client.pydb
 shirtCol = mongoDB.shirts
 
@@ -17,58 +19,63 @@ cursor = db.cursor()
 @route('/shirt/<shirtID>', method='GET')
 def getShirt( shirtID=1234 ):
 	doc = shirtCol.find_one({"shirtId":shirtID},{"_id":0})
+	doc['createdDate'] = doc['createdDate'].strftime("%Y-%m-%d %H:%M:%S")
 	return doc
+	#return json.dumps(doc, default=json_util.default)
 
 @route('/shirts', method="PUT")
 def updateShirt():
 	doc = request.json
 	print "doc is %s" % doc
 	shirtCol.update({"shirtId": doc['shirtId']}, doc)
-	return { "success" : True}
+	return { "success" : "OK"}
 
 @route('/shirts', method='DELETE' )
 def deleteShirts():
 	doc = request.json
 	shirtCol.remove({"shirtId" : doc['shirtId']})
-	return { "success" : True}
+	return { "success" : "OK"}
 
 @route('/shirts', method='POST')
 def insertShirt():
 	
 	#print "inside post"
 	doc = request.json
+	doc['createdDate'] = datetime.datetime.now();
+	print "doc is %s" % doc
 	id = shirtCol.insert(doc)
-	
-	return { "success" : True }
+	return { "success" : "OK" }
 
 @route('/shoe/<shoeId>', method='GET')
 def getShirt(shoeId="1"):
 	
-	cursor.execute("SELECT shoeId, shoeName, shoeQuantity, createdBy FROM SHOE_TABLE where shoeId = %s" % shoeId)
+	cursor.execute("SELECT shoeId, shoeName, shoeQuantity, createdBy, createdDate FROM SHOE_TABLE where shoeId = %s" % shoeId)
 	
 	row = cursor.fetchone()
 	
-	return {"shoeId": row[0], "shoeName": row[1], "shoeQuantity": row[2], "createdBy": row[3]}
+	return {"shoeId": row[0], "shoeName": row[1], "shoeQuantity": row[2], "createdBy": row[3], "createdDate": row[4]}
 
 @route('/shoes', method="PUT")
 def updateShirt():
 	doc = request.json
 	
-	return { "success" : True}
+	return { "success" : "OK"}
 
 @route('/shoes', method='DELETE' )
 def deleteShirts():
 	doc = request.json
 	cursor.execute("DELETE From SHOE_TABLE where shoeId= %s" %doc['shoeId'])
-	return { "success" : True}
+	return { "success" : "OK"}
 
 @route('/shoes', method='POST')
 def insertShirt():
 	
 	doc = request.json
-	query = "Insert into SHOE_TABLE (shoeId, shoeName, shoeQuantity, createdBy) values (%s, %s, %s, %s)"
-	cursor.execute(query, (doc['shoeId'], doc['shoeName'], doc['shoeQuantity'], doc['createdBy']))	
-	return { "success" : True }
+	query = "Insert into SHOE_TABLE (shoeId, shoeName, shoeQuantity, createdBy, createdDate) values (%s, %s, %s, %s, %s)"
+	cursor.execute(query, (doc['shoeId'], doc['shoeName'], doc['shoeQuantity'], doc['createdBy'], datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))	
+	db.commit()
+	return { "success" : "OK" }
 
-run(host='localhost', port=8080, debug=True)
+if __name__ == "__main__":
+	run(host='localhost', port=8080)
 	
